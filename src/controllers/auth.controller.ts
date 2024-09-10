f39c7813c65dbe850b35e66e0dfb68965a08669e
MAC <horlarmihleykan10@gmail.com>
2024-09-11 00:22:37 +0100

    fix: user

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -12,45 +12,30 @@ const signUp = asyncHandler(async (req: Request, res: Response) => {
 });
 
 
-// const login = asyncHandler(async (req: Request, res: Response) => {
+const login = asyncHandler(async (req: Request, res: Response) => {
 
-//     const {users, message, access_token} = await authService.login(req.body);
-//     sendJsonResponse(res, 200, message, users, access_token)
+    const {user, message, access_token} = await authService.login(req.body);
+    sendJsonResponse(res, 200, message, user, access_token)
 
-// });
+});
 
-const login = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      const { users } = await authService.login(req.body);
-      return res.status(200).json({ users });
-    } catch (error) {
-      next(error);
-    }
-};
+// const login = async (req: Request, res: Response, next: NextFunction) => {
+//     try {
+//       const { users } = await authService.login(req.body);
+//       return res.status(200).json({ users });
+//     } catch (error) {
+//       next(error);
+//     }
+// };
 
 const getUsers = asyncHandler( async (req: Request, res: Response) => {
    
     const users = await authService.getUsers();
     const allusers = { users };
-    return res.status(200).json({ allusers });
-
-    // sendJsonResponse(res, 200, "Successfully retrievedddd users", allusers);
+    sendJsonResponse(res, 200, "Successfully retrievedddd users", allusers);
   
 });
 
-// const getUsers = async (req: Request, res: Response) => {
-//     try {
-//       const users = await authService.getUsers();
-//       if (!users) {
-//         return sendJsonResponse(res, 500, "Could not get users");
-//       }
-//       const allusers = { users };
-//       sendJsonResponse(res, 200, "Successfully retrieved users", allusers);
-//     } catch (error) {
-//       return sendJsonResponse(res, error.status || 500, error.message || error);
-//     }
-// };
-  
 
 
 export { signUp, login, getUsers};
