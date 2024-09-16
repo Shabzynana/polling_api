504952ba28b875b026b1432ca27f6dd4c15d6078
MAC <horlarmihleykan10@gmail.com>
2024-09-16 21:21:19 +0100

    fix: docs

--- src/docs/user.docs.ts
+++ src/docs/user.docs.ts
@@ -41,3 +41,47 @@ export const getAllUsers = `
  *         description: Internal server error.
  */
 `;
+
+
+export const cuurentUser = `
+/**
+ * @swagger
+ * /api/v1/current_user:
+ *   get:
+ *     summary: Get Current User
+ *     description: Retrieve details of the current loggedin User
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: Successfully retrieved Curent User.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status_code:
+ *                   type: integer
+ *                   example: 200
+ *                 message:
+ *                   type: string
+ *                   example: "Current Logedin User"
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     user:
+ *                       type: object
+ *                       properties:
+ *                         id:
+ *                           type: integer
+ *                         username:
+ *                           type: string
+ *                         email:
+ *                           type: string
+ *       500:
+ *         description: Internal server error.
+ */
+`;
+
+
+
+
