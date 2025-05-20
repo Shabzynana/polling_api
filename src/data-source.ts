36770f558ff4dbb7d09ad08d83a4e2adff2fe6bb
MAC <horlarmihleykan10@gmail.com>
2025-05-20 19:25:29 +0100

    fix: data-source

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,6 +4,32 @@ import config from "./config";
 
 const isDevelopment = config.NODE_ENV === "development";
 
+const rootDir = isDevelopment ? "src" : "dist";
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: config.POSTGRES_HOST,
+  port: Number(config.POSTGRES_PORT) || 5432,
+  username: config.POSTGRES_USER,
+  password: config.POSTGRES_PASSWORD,
+  database: config.POSTGRES_DB,
+  synchronize: isDevelopment,
+  logging: false,
+  entities: [`${rootDir}/models/**/*.{ts,js}`], 
+  migrations: [`${rootDir}/db/migrations/**/*.{ts,js}`],
+  migrationsTableName: "migrations",
+  ssl: true,
+});
+
+export async function initializeDataSource() {
+  if (!AppDataSource.isInitialized) {
+    await AppDataSource.initialize();
+  }
+  return AppDataSource;
+}
+
+export default AppDataSource;
+
 // const AppDataSource = new DataSource({
 //   type: "postgres",
 //   host: config.POSTGRES_HOST,
@@ -19,29 +45,3 @@ const isDevelopment = config.NODE_ENV === "development";
 //   // ssl: isDevelopment,
 //   ssl: true,
 // });
-
-const rootDir = isDevelopment ? "src" : "dist";
-
-const AppDataSource = new DataSource({
-  type: "postgres",
-  host: config.POSTGRES_HOST,
-  port: Number(config.POSTGRES_PORT) || 5432,
-  username: config.POSTGRES_USER,
-  password: config.POSTGRES_PASSWORD,
-  database: config.POSTGRES_DB,
-  synchronize: isDevelopment,
-  logging: false,
-  entities: [`${rootDir}/models/**/*.{ts,js}`],  // ✅ Support both .ts and .js
-  migrations: [`${rootDir}/db/migrations/**/*.{ts,js}`],
-  migrationsTableName: "migrations",
-  ssl: true,
-});
-
-export async function initializeDataSource() {
-  if (!AppDataSource.isInitialized) {
-    await AppDataSource.initialize();
-  }
-  return AppDataSource;
-}
-
-export default AppDataSource;
